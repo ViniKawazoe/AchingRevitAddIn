541b75700b6fd77e24cb5f13e8f29d71df9de2cf
Vinicius Kawazoe <vinikawazoe@gmail.com>
2020-08-18 20:02:46 -0300

    Project created

---
+++ AchingRevitAddIn/App.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace AchingRevitAddIn
+{
+    public class App
+    {
+    }
+}
