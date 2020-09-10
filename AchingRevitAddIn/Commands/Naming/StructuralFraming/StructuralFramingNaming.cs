f63d9473b3435b56cada3526034e350c2b7c48b5
Vinicius Kawazoe <vinikawazoe@gmail.com>
2020-09-09 21:43:32 -0300

    Started to implement the Structural Framing Naming class

--- AchingRevitAddIn/Commands/Naming/StructuralFraming/StructuralFramingNaming.cs
+++ AchingRevitAddIn/Commands/Naming/StructuralFraming/StructuralFramingNaming.cs
@@ -15,9 +15,31 @@ namespace AchingRevitAddIn
     [Transaction(TransactionMode.Manual)]
     class StructuralFramingNaming : IExternalCommand
     {
+        private static UIDocument Uidoc { get; set; }
+
+        #region public methods
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            // Get UIDocument and Document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            // Send it to the public variable so other methods can call it
+            Uidoc = uidoc;
+
+            // Only allow view plans, 3D views, sections and elevations
+            if (uidoc.ActiveView as ViewPlan == null &&
+                uidoc.ActiveView as View3D == null &&
+                uidoc.ActiveView as ViewSection == null)
+            {
+                return Result.Failed;
+            }
+
+            // Show UI
+
             return Result.Succeeded;
         }
+
+
+        #endregion
     }
 }
