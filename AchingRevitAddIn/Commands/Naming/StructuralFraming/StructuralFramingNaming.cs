aced8509fe7fcde4410d16d4c8083194440587bb
Vinicius Kawazoe <vinikawazoe@gmail.com>
2020-09-12 19:12:17 -0300

    Created the sort framings method

--- AchingRevitAddIn/Commands/Naming/StructuralFraming/StructuralFramingNaming.cs
+++ AchingRevitAddIn/Commands/Naming/StructuralFraming/StructuralFramingNaming.cs
@@ -61,8 +61,8 @@ namespace AchingRevitAddIn
                 // Convert References to Elements
                 IList<Element> strFramings = pickedReferences.Select(x => doc.GetElement(x)).ToList();
 
-                IList<Element> horizontalFramings = null;
-                IList<Element> verticalFramings = null;
+                IList<Element> horizontalFramings = new List<Element>();
+                IList<Element> verticalFramings = new List<Element>();
 
                 // Separate horizontal and vertical beams 
                 foreach (Element beam in strFramings)
@@ -72,8 +72,13 @@ namespace AchingRevitAddIn
                     XYZ endPoint = ((LocationCurve)beam.Location).Curve.GetEndPoint(1);
 
                     XYZ vector = (endPoint - startPoint).Normalize();
+                    double vX = vector.X;
+                    double vY = vector.Y;
 
-                    if (vector.X >= vector.Y)
+                    if (vX < 0) { vX *= -1; }
+                    if (vY < 0) { vY *= -1; }
+
+                    if (vX >= vY)
                     {
                         horizontalFramings.Add(beam);
                     }
@@ -83,26 +88,142 @@ namespace AchingRevitAddIn
                     }
                 }
 
-                // Grouping the beams
+                // Sort by center point
+                SortFramings(horizontalFramings, verticalFramings, sortHorizontal, sortVertical);
+
+                // Join both lists
+                IList<Element> sortedFramings = new List<Element>();
+
                 if (horizontalFramings != null)
                 {
-                    // Group the continuous beams
+                    foreach (Element framing in horizontalFramings)
+                    {
+                        sortedFramings.Add(framing);
+                    }
                 }
-
                 if (verticalFramings != null)
                 {
-                    // Group the continuous beams
+                    foreach (Element framing in verticalFramings)
+                    {
+                        sortedFramings.Add(framing);
+                    }
                 }
+
+                string s = "";
+                int count = 1;
+                /*
+                foreach (Element fm in sortedFramings)
+                {
+                    s = s + "Framing " + count
+                        + "\n"
+                        + "Start point: " + ((LocationCurve)fm.Location).Curve.GetEndPoint(0).ToString()
+                        + "\n"
+                        + "Center point: " + ((LocationCurve)fm.Location).Curve.Evaluate(0.5, true).ToString()
+                        + "\n"
+                        + "SEnd point: " + ((LocationCurve)fm.Location).Curve.GetEndPoint(1).ToString()
+                        + "\n"
+                        + "-------------------------------------------------------"
+                        + "\n";
+
+                    count++;
+                }
+                */
+                for (int i = 0; i < sortedFramings.Count; i++)
+                {
+                    s = s + "Framing " + count
+                        + "\n"
+                        + "Start point: " + ((LocationCurve)sortedFramings[i].Location).Curve.GetEndPoint(0).ToString()
+                        + "\n"
+                        + "Center point: " + ((LocationCurve)sortedFramings[i].Location).Curve.Evaluate(0.5, true).ToString()
+                        + "\n"
+                        + "SEnd point: " + ((LocationCurve)sortedFramings[i].Location).Curve.GetEndPoint(1).ToString()
+                        + "\n"
+                        + "-------------------------------------------------------"
+                        + "\n";
+
+                    count++;
+                }
+
+                TaskDialog.Show("Framings", s);
             }
             catch
             {
             }
         }
 
-        static internal IList<Element> GroupBeams(IList<Element> beamList)
+        /// <summary>
+        ///  Sort framings
+        /// </summary>
+        /// <param name="horizontalFramings"></param>
+        /// <param name="verticalFramings"></param>
+        /// <param name="sortHorizontal"></param>
+        /// <param name="sortVertical"></param>
+        static internal void SortFramings(IList<Element> horizontalFramings, IList<Element> verticalFramings, int sortHorizontal, int sortVertical)
         {
-            IList<Element> list = null;
-            return list;
+            if (sortVertical == 0 && sortHorizontal == 0)
+            {
+                if (horizontalFramings != null)
+                {
+                    horizontalFramings.
+                        OrderByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y).
+                        ThenBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X);
+                }
+
+                if (verticalFramings != null)
+                {
+                    verticalFramings.
+                        OrderBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X).
+                        ThenBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y);
+                }
+            }
+            if (sortVertical == 0 && sortHorizontal == 1)
+            {
+                if (horizontalFramings != null)
+                {
+                    horizontalFramings.
+                        OrderByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y).
+                        ThenByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X);
+                }
+
+                if (verticalFramings != null)
+                {
+                    verticalFramings.
+                        OrderBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X).
+                        ThenByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y);
+                }
+            }
+            if (sortVertical == 1 && sortHorizontal == 0)
+            {
+                if (horizontalFramings != null)
+                {
+                    horizontalFramings.
+                        OrderBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y).
+                        ThenBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X);
+                }
+
+                if (verticalFramings != null)
+                {
+                    verticalFramings.
+                        OrderByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X).
+                        ThenBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y);
+                }
+            }
+            if (sortVertical == 1 && sortHorizontal == 1)
+            {
+                if (horizontalFramings != null)
+                {
+                    horizontalFramings.
+                        OrderBy(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y).
+                        ThenByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X);
+                }
+
+                if (verticalFramings != null)
+                {
+                    verticalFramings.
+                        OrderByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).X).
+                        ThenByDescending(k => ((LocationCurve)k.Location).Curve.Evaluate(0.5, true).Y);
+                }
+            }
         }
 
         #endregion
